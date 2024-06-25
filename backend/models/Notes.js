cac09950b36e15159caa7e2d383ba511bf98404d
mhk0603 <mahekjthakkar123@gmail.com>
2024-06-25 14:38:26 +0530

    REACT VID 43 COMPLETE

---
+++ backend/models/Notes.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const NotesSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+        
+    },
+    tag:{
+        type: String,
+        default: "General"
+    },
+    date:{
+        type: String,
+        default: Date.now      //dont call it here using ()
+    },
+
+
+    
+  });
+
+  module.exports=mongoose.model('user', NotesSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: String,
+        default: Date.now      //dont call it here using ()
+    },
+
+
+    
+  });
+
+  module.exports=mongoose.model('user', UserSchema);
