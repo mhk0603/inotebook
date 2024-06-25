7f9074ffcfac9048b615c1bf8648267dbf7170c6
mhk0603 <mahekjthakkar123@gmail.com>
2024-06-26 00:18:09 +0530

    vid 46 backend changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,25 @@
 const express=require('express');
 const router=express.Router();
 const User=require('../models/User')
+const { body, validationResult } = require('express-validator');
 
 // Create a user using: POST "/api/auth". Doesnt require Authentication
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    const newUser= new User(req.body);
-    newUser.save()
-    res.send(req.body)
+router.post('/',[
+    body('email','Enter a valid email').isEmail(),
+    body('name','Enter a valid name').isLength({min:3}),
+    body('password', 'Password must be atleast 5 characters').isLength({min:5}),
+],(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user=>res.json(user));
+    
+    
 })
 
 module.exports=router
