b82e3e37989a845d6e2593ff2044eb777b4519d3
mhk0603 <mahekjthakkar123@gmail.com>
2024-06-26 16:08:11 +0530

    vid 49 comp backend changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json()) // to use req.body u need to write this middleware then u will not get undefined in console and now u can deal in json
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,25 +1,48 @@
-const express=require('express');
-const router=express.Router();
-const User=require('../models/User')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// Create a user using: POST "/api/auth". Doesnt require Authentication
-router.post('/',[
-    body('email','Enter a valid email').isEmail(),
-    body('name','Enter a valid name').isLength({min:3}),
-    body('password', 'Password must be atleast 5 characters').isLength({min:5}),
-],(req,res)=>{
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+const JWT_SECRET = 'Mahek@tha';
+
+// Create a user using: POST "/api/auth/createuser". Doesn't require Authentication
+router.post('/createuser', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    // If there are errors, return bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user=>res.json(user));
-    
-    
-})
 
-module.exports=router
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt); // await as it returns a promise
+
+        // Create a new user
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass,
+        });
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        };
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+        
+        res.json({authToken});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occurred");
+    }
+});
+
+module.exports = router;
